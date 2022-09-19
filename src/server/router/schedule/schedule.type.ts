51812d4ec8851fbcebf1508db6d6dd61df6415a9
Leangsuor kim <leangsourkim098@gmail.com>
2022-09-19 21:04:05 +0700

    update schedule

--- src/server/router/schedule/schedule.type.ts
+++ src/server/router/schedule/schedule.type.ts
@@ -1,3 +1,4 @@
+import { TypeOf } from "yup";
 import * as z from "zod";
 
 export const bookingSchema = z.object({
@@ -20,7 +21,28 @@ export const updateAppointmentSchema = z.object({
   id: z.string(),
 });
 
+export const updateUpcomingAppointmentSchema = z.object({
+  id: z.string(),
+  senderId: z.string(),
+});
+
+export const updateUpcomingApprovalAppointmentSchema = z.object({
+  id: z.string(),
+});
+
+export const deleteUpcomingApprovalAppointmentSchema = z.object({
+  id: z.string(),
+});
+
+export const deleteUpcomingAppointmentSchema = z.object({
+  id: z.string(),
+});
+
 export type IBooking = z.infer<typeof bookingSchema>;
 export type IAppointment = z.infer<typeof appointmentSchema>;
 export type IDeleteAppointment = z.infer<typeof deleteAppointmentSchema>;
 export type IUpdateAppointment = z.infer<typeof updateAppointmentSchema>;
+export type IUpdateUpcomingAppointment = z.infer<typeof updateUpcomingAppointmentSchema>;
+export type IUpdateUpcomingApprovalAppointment = z.infer<typeof updateUpcomingApprovalAppointmentSchema>;
+export type IDeleteUpcomingApprovalAppointment = z.infer<typeof deleteUpcomingApprovalAppointmentSchema>;
+export type IDeleteUpcomingAppointment = z.infer<typeof deleteUpcomingAppointmentSchema>;
